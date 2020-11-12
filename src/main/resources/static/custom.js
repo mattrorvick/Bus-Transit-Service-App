c94aa048c99de8f23bf4be397d6594efbc4d1ffa
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-11 21:38:09 -0600

    fixing bus and user location icons

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,43 +1,53 @@
 
-let map;
-let marker;
 
-
-let contentString = "<h2> Vehicle#: " + vehicleNum + "</h2>";
 
 function initMap() {
 
-    
-    
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
+
+    let image = {url: '/busicon.png', scaledSize: new google.maps.Size(50,50) };
+
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) }, 
         zoom: 15,
         scrollwheel: false
     });
     
-    let image = {url: '/busicon.png', scaledSize: new google.maps.Size(40,40)};
+    console.log(userLocation);
 
     let userMarker = new google.maps.Marker({
-        position: { lat: parseFloat(userLocation[i].lat), lng: parseFloat(userLocation[i].lng) },
+        position: { lat: parseFloat(userLocation.lat), lng: parseFloat(userLocation.lng) },
         map: map,
-        marker: marker,
+        
     });
 
     for (i=0; i<busLocations.length; i++){
-        
+        vehicleNum = busLocations[i].VEHICLE;
+        let contentString = "<h2> Vehicle#: " + vehicleNum + "</h2>";
+        let infoWindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        console.log(busLocations);
 
         let marker = new google.maps.Marker({
             position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
             map: map,
+            
             icon: image,
         });
+
+        marker.addListener("click", () => {
+            infoWindow.open(map, marker);
+        }); 
+        
     }
 
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.open(map, marker);
-    });
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     infoWindow.open(map, marker);
+    // });
+    
 
     
 }
